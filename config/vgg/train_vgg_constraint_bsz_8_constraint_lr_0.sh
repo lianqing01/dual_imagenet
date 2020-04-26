188f64b3e00c430bfdda1ac1af261f4f733fb998
lianqing11 <lianqing11@foxmail.com>
2020-04-26 14:06:13 +0000

    update

--- config/vgg/train_vgg_constraint_bsz_8_constraint_lr_0.sh
+++ config/vgg/train_vgg_constraint_bsz_8_constraint_lr_0.sh
@@ -1,7 +1,7 @@
 python train_constraint_bn_v2_1.py --model vgg16_constraint_bn_v2 \
     --log_dir vgg/vgg16_constraint_bsz_8_lr_0 \
     --lr 0.05 \
-    --batch_size 8 \
+    --batch-size 8 \
     --constraint_lr 0 \
     --constraint_decay 0 \
     --lambda_constraint_weight 0 \
