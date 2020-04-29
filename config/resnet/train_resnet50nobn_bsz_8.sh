f9ad71c1923fb4f6b445d241e38b884974dc7086
lianqing11 <lianqing11@foxmail.com>
2020-04-29 02:42:01 +0000

    update

--- config/resnet/train_resnet50nobn_bsz_8.sh
+++ config/resnet/train_resnet50nobn_bsz_8.sh
@@ -1,4 +1,4 @@
-python train.py --model resnet_nobn101 \
+python train.py --model resnet50 \
     --log_dir resnet/oracle_resnet_nobn101_bsz_8_bn \
-    --batch-size 8 \
-    --lr 0.005 \
+    --batch-size 128 \
+    --lr 1 \
