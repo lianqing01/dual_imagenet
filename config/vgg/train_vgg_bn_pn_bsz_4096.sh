21e3ab4f27bc1b231281a90e7d3dbfe562d59a4c
LIAN <qlianab@gpu9.math.ust.hk>
2020-05-12 09:58:38 +0800

    update

--- config/vgg/train_vgg_bn_pn_bsz_4096.sh
+++ config/vgg/train_vgg_bn_pn_bsz_4096.sh
@@ -1,5 +1,5 @@
 python train_pn.py --model vgg16_bn \
-    --log_dir vgg/vgg16_pn_bsz_128_pn-bsz_128 \
-    --pn-batch-size 128 \
+    --log_dir vgg/vgg16_pn_bsz_128_pn-bsz_4096 \
+    --pn-batch-size 512 \
     --batch-size 128 \
     --lr 0.05 \
