8abcfc2614b55b67522cfacaa3d5ace73093cf23
lianqing <lianqing11@foxmail.com>
2020-05-26 05:05:01 +0800

    auto

--- config/vgg_new/train_vgg_constraint_bsz_4_ind_1e-1_1e-1.sh
+++ config/vgg_new/train_vgg_constraint_bsz_4_ind_1e-1_1e-1.sh
@@ -1,7 +1,8 @@
 python train_constraint_bn_v2_1.py --model vgg16_constraint_bn_v2 \
-    --log_dir vgg/vgg16_constraint_lr1e-1_bsz_128_lr_1e-2_weight_1e-3_noise_ind_1e-1_1e-1_clamp \
-    --lr 0.1 \
-    --constraint_lr 0.01 \
+    --log_dir vgg/vgg16_constraint_bsz_4_noise_ind_1e-1_1e-1 \
+    --lr 0.003125 \
+    --constraint_lr 0.0003125 \
+    --batch-size 4 \
     --constraint_decay 1 \
     --lambda_constraint_weight 0.001 \
     --noise_data_dependent False \
