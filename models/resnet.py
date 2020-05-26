8d3c4b091884caa97362ac244616d2d2928bc3f0
lianqing <lianqing11@foxmail.com>
2020-05-26 15:20:46 +0800

    auto

--- models/resnet.py
+++ models/resnet.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 from torchvision.models.utils import load_state_dict_from_url
-from .batchnorm import BatchNorm2d
+from torch.nn import BatchNorm2d
 
 __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
            'resnet152', 'resnext50_32x4d', 'resnext101_32x8d',
