1a8de720e60dabfe0cf440b312bc2ab6646d6e47
lianqing <lianqing11@foxmail.com>
2020-05-28 19:34:12 +0800

    auto

--- config/imagenet/train_vgg.sh
+++ config/imagenet/train_vgg.sh
@@ -1,1 +1,1 @@
-python -m torch.distributed.launch --nproc_per_node=$1 main_amp.py -a resnet18 --b 256 --workers 8 --opt-level O1  ./data/imagenet --sync_bn
+python -m torch.distributed.launch --nproc_per_node=$1 main_amp.py -a resnet18 --b 512 --workers 8 --opt-level O1  ./data/imagenet --sync_bn
