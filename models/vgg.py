68b0593ad937a5c0897cff54a7c2c24f48e4db55
lianqing11 <lianqing1997@gmail.com>
2020-06-01 16:39:41 +0800

    auto

--- models/vgg.py
+++ models/vgg.py
@@ -101,7 +101,7 @@ class VGG(nn.Module):
                                 nn.ReLU(inplace=True)]
                 elif with_bn =='gn':
                     layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
-                                nn.GroupNorm(64, x, affine=True),
+                                nn.GroupNorm(32, x, affine=True),
                                nn.ReLU(inplace=True)]
                 elif with_bn == 'constraint_bn_v2_no_affine':
                     if idx == 0:
