efce161633b35bdf018a8daf96dd256137966593
lianqing <lianqing11@foxmail.com>
2020-06-01 16:43:26 +0800

    auto

--- models/resnet_constraintbn.py
+++ models/resnet_constraintbn.py
@@ -23,12 +23,12 @@ model_urls = {
 def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
     """3x3 convolution with padding"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=dilation, groups=groups, bias=False, dilation=dilation)
+                     padding=dilation, groups=groups, bias=True, dilation=dilation)
 
 
 def conv1x1(in_planes, out_planes, stride=1):
     """1x1 convolution"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=True)
 
 
 class BasicBlock(nn.Module):
@@ -191,8 +191,6 @@ class resnet_constraint(nn.Module):
         # See note [TorchScript super()]
         x = self.conv1(x)
         x = self.relu(x)
-
-        x = self.bn1(x)
         x = self.maxpool(x)
 
         x = self.layer1(x)
