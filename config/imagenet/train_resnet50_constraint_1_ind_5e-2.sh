e10e8e1e13613dd1f59396c92e611118d85e4194
lianqing11 <lianqing1997@gmail.com>
2020-06-06 17:45:18 +0800

    auto

--- config/imagenet/train_resnet50_constraint_1_ind_5e-2.sh
+++ config/imagenet/train_resnet50_constraint_1_ind_5e-2.sh
@@ -1,4 +1,4 @@
-python -m torch.distributed.launch --nproc_per_node=$1 --master_port=1222 main_amp_constraint.py -a resnet_constraint50 --b 512 --workers 8 --opt-level O1  \
+python -m torch.distributed.launch --nproc_per_node=$1 --master_port=1222 main_amp_constraint.py -a resnet_constraint50 --b 480 --workers 8 --opt-level O1  \
     ./data/imagenet \
     --log_dir imagenet/constraint_50_5e-2_ind_5e-2 \
     --constraint_lr 0.01 \
