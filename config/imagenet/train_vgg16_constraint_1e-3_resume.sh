cc06d7ad8a5a1fff2926745fb841a8971290048e
lianqing11 <lianqing1997@gmail.com>
2020-06-09 10:48:49 +0800

    auto

---
+++ config/imagenet/train_vgg16_constraint_1e-3_resume.sh
@@ -0,0 +1,16 @@
+python -m torch.distributed.launch --nproc_per_node=$1 --master_port=3622 main_amp_constraint_resume.py -a vgg_imagenet16_constraint --b 512 --workers 8 --opt-level O2  \
+    ./data/imagenet \
+    --log_dir imagenet/vgg_imagenet16_constraint_1e-3 \
+    --lr 0.1 \
+    --constraint_lr 0.005 \
+    --constraint_decay 1 \
+    --epochs 120 \
+    --lambda_constraint_weight 1e-3 \
+    --resume results/imagenet/vgg_imagenet16_constraint_1e-3_Mon-Jun--8-11:16:09-2020/89_checkpoint.pth.tar \
+    --lambda_weight_mean 5 \
+    --decrease_affine_lr 1 \
+    --sample_noise True \
+    --noise_data_dependent False \
+    --noise_mean_std 0.05 \
+    --noise_var_std 0.05 \
+
